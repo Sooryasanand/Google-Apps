247d777793d8a9419863debd5d3d06bda56a8f0a
Sooryasanand <sooryanarayanan.com@gmail.com>
2021-09-27 23:20:57 +1000

    Finished Subscriptions Page

--- src/SubscriptionScreen.js
+++ src/SubscriptionScreen.js
@@ -6,58 +6,86 @@ import {
   SafeAreaView,
   FlatList,
   Image,
+  ImageBackground,
 } from "react-native";
 import Header from "./components/Header";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import HeaderSubTab from "./components/HeaderSubTab";
+import HeaderTopTab from "./components/HeaderTopTab";
+import { ScrollView } from "react-native-gesture-handler";
+import HeaderTopTabSub from "./components/HeaderTopTabSub";
 
 const DATA = [
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "I spent 50 Hours in Prison",
+    title: "1 vs 10 Hunters in GTA!",
+    creator: "MrBeast Gaming",
+    time: "10:05",
     imageUrl:
-      "https://i.ytimg.com/vi/nM89Wl03Q4g/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA9WIrGCwdI6am1QbaZeUfaXygVXQ",
+      "https://i.ytimg.com/vi/xwdhHTtc-3w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCpemt8kKZhBhSoErqA2XdyrJhu3w",
     channel:
-      "https://yt3.ggpht.com/ytc/AKedOLSdjk3yfmEPE7rf2rQTHWvSMx-2sQ59M4kRh9SWSQ=s176-c-k-c0x00ffffff-no-rj",
+      "//yt3.ggpht.com/ytc/AKedOLRvgsd_ky0wkF1xLv6jbESo5mBQZWfn8lkTQbOcfQ=s176-c-k-c0x00ffffff-no-rj-mo",
   },
   {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Hit The Target, Win $300,000",
+    title: "Google – How to Start a New Job",
+    creator: "Google",
+    time: "0:31",
     imageUrl:
-      "https://i.ytimg.com/vi/NZlClr_ivb4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBUchtvs9Prx09JbJCZEMKAlrhQDA",
+      "https://i.ytimg.com/vi/uzHYX1CmO6A/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCrR96Z0zV8dnhraSzNrZit8bPWUQ",
     channel:
-      "https://yt3.ggpht.com/ytc/AKedOLSdjk3yfmEPE7rf2rQTHWvSMx-2sQ59M4kRh9SWSQ=s176-c-k-c0x00ffffff-no-rj",
+      "//yt3.ggpht.com/ytc/AKedOLQybyN1mVv2t2XhNQKweDRXwqdf6t6sAHZWwh19Zdw=s176-c-k-c0x00ffffff-no-rj-mo",
   },
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Anything You Can Fit In The Triangle I’ll Pay For",
+    title: "The BIGGEST Tech Hoarder!",
+    creator: "Linus Tech Tips",
+    time: "10:05",
     imageUrl:
-      "https://i.ytimg.com/vi/E6E22XQPhhg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD4G7vzHqiFBBElGr4V2zBOwzubVw",
+      "https://i.ytimg.com/vi/cw7_dWogBIU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCkBHaUAgzzJWtsubMfBL05Fucncg",
     channel:
-      "https://yt3.ggpht.com/ytc/AKedOLSdjk3yfmEPE7rf2rQTHWvSMx-2sQ59M4kRh9SWSQ=s176-c-k-c0x00ffffff-no-rj",
+      "https://yt3.ggpht.com/ytc/AKedOLSGE5pCFbd5alFmWeOJZPNjsEhkN1FCxFdYjaE3Og=s68-c-k-c0x00ffffff-no-rj",
   },
   {
     id: "58694a0f-3da1-471f-bd96-11e29d72",
-    title: "I Got Hunted By A Real Bounty Hunter",
+    title: "How to Learn Anything FASTER",
+    time: "14:43",
+    creator: "Ali Abdaal",
     imageUrl:
-      "https://i.ytimg.com/vi/TQHEJj68Jew/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDG2dQ5h-UfhfPyFUEq2fDHjLa83w",
+      "https://i.ytimg.com/vi/unityETmypk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDlAjKGmwF2sKnQjTLLv_St3Lt6-g",
     channel:
-      "https://yt3.ggpht.com/ytc/AKedOLSdjk3yfmEPE7rf2rQTHWvSMx-2sQ59M4kRh9SWSQ=s176-c-k-c0x00ffffff-no-rj",
+      "https://yt3.ggpht.com/ytc/AKedOLSWd0-HAZHehT9aad84m27uDgIAi86IS6sGsmTH=s68-c-k-c0x00ffffff-no-rj",
   },
   {
     id: "58694a0f-3d1f-bd96-145571e29d72",
-    title: "Extreme $100,000 Game of Tag!",
+    title: "🔴 Build Tesla Clone with React Native",
+    time: "20:34",
+    creator: "Clever Programmer",
     imageUrl:
-      "https://i.ytimg.com/vi/WcwGleN38zE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAYGFz9ePvZvaXO6P2bX9AmQyZrkQ",
+      "https://i.ytimg.com/vi/y7-IG4SSI3E/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB0kAjWozIgCjBwRk9vmI3_1rsvdg",
     channel:
-      "https://yt3.ggpht.com/ytc/AKedOLSdjk3yfmEPE7rf2rQTHWvSMx-2sQ59M4kRh9SWSQ=s176-c-k-c0x00ffffff-no-rj",
+      "https://yt3.ggpht.com/ytc/AKedOLRbdv3Di8paQyrgMF_VwFXPkhwVzcW59Vgo8dTsyw=s88-c-k-c0x00ffffff-no-rj",
   },
 ];
 
-function Item({ title, imageUrl, channel }) {
+function Item({ title, imageUrl, channel, creator, time }) {
   return (
     <View style={styles.item}>
-      <Image source={{ uri: imageUrl }} style={{ height: 200 }} />
+      <ImageBackground source={{ uri: imageUrl }} style={{ height: 200 }}>
+        <Text
+          style={{
+            position: "absolute",
+            marginTop: 170,
+            marginLeft: 330,
+            backgroundColor: "black",
+            padding: 2,
+            color: "white",
+            fontSize: 13,
+          }}
+        >
+          {time}
+        </Text>
+      </ImageBackground>
       <View
         style={{
           display: "flex",
@@ -73,7 +101,7 @@ function Item({ title, imageUrl, channel }) {
         >
           <Image
             source={{ uri: channel }}
-            style={{ height: 60, width: 60, borderRadius: 30, margin: 10 }}
+            style={{ height: 30, width: 30, borderRadius: 30, margin: 10 }}
           />
           <View>
             <Text style={styles.title}>{title}</Text>
@@ -81,17 +109,10 @@ function Item({ title, imageUrl, channel }) {
               style={{
                 paddingLeft: 6,
                 paddingTop: 4,
-                color: "gray",
+                color: "#606060",
               }}
             >
-              MrBeast . {parseInt(Math.random() * 1000)}K Views
-            </Text>
-            <Text
-              style={{
-                paddingLeft: 6,
-                color: "gray",
-              }}
-            >
+              {creator} · {parseInt(Math.random() * 1000)}K Views ·{" "}
               {parseInt(Math.random() * 10)} months ago
             </Text>
           </View>
@@ -99,8 +120,8 @@ function Item({ title, imageUrl, channel }) {
         <Icon
           name="dots-vertical"
           size={25}
-          color="gray"
-          style={{ paddingTop: 20 }}
+          color="#606060"
+          style={{ paddingTop: 10 }}
         />
       </View>
     </View>
@@ -110,19 +131,24 @@ function Item({ title, imageUrl, channel }) {
 function ExploreScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Header />
-      <HeaderSubTab />
-      <FlatList
-        data={DATA}
-        renderItem={({ item }) => (
-          <Item
-            title={item.title}
-            imageUrl={item.imageUrl}
-            channel={item.channel}
-          />
-        )}
-        keyExtractor={(item) => item.id}
-      />
+      <ScrollView>
+        <Header />
+        <HeaderSubTab />
+        <HeaderTopTabSub />
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => (
+            <Item
+              title={item.title}
+              time={item.time}
+              imageUrl={item.imageUrl}
+              channel={item.channel}
+              creator={item.creator}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -133,14 +159,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   item: {
-    height: 300,
+    height: 270,
     borderStyle: "solid",
-    borderBottomColor: "gray",
+    borderBottomColor: "grey",
   },
   title: {
-    fontSize: 20,
+    fontSize: 15,
+    fontWeight: "500",
     paddingLeft: 6,
-    paddingTop: 10,
+    paddingTop: 8,
     shadowOpacity: 0,
   },
 });
--- src/components/HeaderSubTab.js
+++ src/components/HeaderSubTab.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  FlatList,
+  Image,
+} from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 const DATA = [
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    creator: "MrBeast Gaming",
+    creator: "MrBeast G...",
     channel:
       "//yt3.ggpht.com/ytc/AKedOLRvgsd_ky0wkF1xLv6jbESo5mBQZWfn8lkTQbOcfQ=s176-c-k-c0x00ffffff-no-rj-mo",
   },
@@ -17,60 +24,98 @@ const DATA = [
   },
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    creator: "Linus Tech Tips",
+    creator: "Linus T...",
     channel:
       "https://yt3.ggpht.com/ytc/AKedOLSGE5pCFbd5alFmWeOJZPNjsEhkN1FCxFdYjaE3Og=s68-c-k-c0x00ffffff-no-rj",
   },
   {
     id: "58694a0f-3da1-471f-bd96-11e29d72",
-    creator: "Ali Abdaal",
+    creator: "Ali Abd...",
     channel:
       "https://yt3.ggpht.com/ytc/AKedOLSWd0-HAZHehT9aad84m27uDgIAi86IS6sGsmTH=s68-c-k-c0x00ffffff-no-rj",
   },
   {
     id: "58694a0f-3d1f-bd96-145571e29d72",
-    creator: "Clever Programmer",
+    creator: "Clever Pr...",
     channel:
       "https://yt3.ggpht.com/ytc/AKedOLRbdv3Di8paQyrgMF_VwFXPkhwVzcW59Vgo8dTsyw=s88-c-k-c0x00ffffff-no-rj",
   },
+  {
+    id: "58694a0f-3d1f-bd96-145571e29d72",
+    creator: "Don't Su...",
+    channel:
+      "https://yt3.ggpht.com/ytc/AKedOLRv2KOma9yXjoxAsKZAFhGfP-HomH7I_PDIVEtO=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "58694a0f-3d1f-bd96-145571e29d72",
+    creator: "Engineer...",
+    channel:
+      "https://yt3.ggpht.com/_2SkjSy2d-0X8qKyImUTZzqHCQmWF6tj5j2VsM7_4SicZDXk5lyiHUUhAbXLuvhu4QllEjGPFg=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "58694a0f-3d1f-bd96-145571e29d72",
+    creator: "Gyasi Lin...",
+    channel:
+      "https://yt3.ggpht.com/ytc/AKedOLSXsZtLxs9RI7NAhDsNikmqoPO6D-AQkHSgbDCdOp0=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "58694a0f-3d1f-bd96-145571e29d72",
+    creator: "iJustine",
+    channel:
+      "https://yt3.ggpht.com/TJTGhrVOoYxtaMg5GL5GoNYkzFrmIieMA4-AeRecbstcc-rWnTcrw8Vt1Ea1t6QBx9o_CIixFP8=s88-c-k-c0x00ffffff-no-rj",
+  },
 ];
 
-function Sub({}) {
+function Item({ creator, channel }) {
   return (
-    <ScrollView
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      style={{ height: 100, backgroundColor: "lightblue", height: 90 }}
-    >
-      <TouchableOpacity style={styles.youtubers}>
-        <Text>MrBeast</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.youtubers}>
-        <Text>Adrian Twarog</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.youtubers}>
-        <Text>Ali Abdaal</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.youtubers}>
-        <Text>Apple</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.youtubers}>
-        <Text>Beast Reacts</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.youtubers}>
-        <Text>Ben Awad</Text>
-      </TouchableOpacity>
-    </ScrollView>
+    <TouchableOpacity style={styles.youtubers}>
+      <Image
+        source={{
+          uri: channel,
+        }}
+        style={{ height: 40, width: 40, borderRadius: 30, margin: 10 }}
+      />
+      <Text numberOfLines={1} ellipsizeMode="tail" style={{ fontSize: 12 }}>
+        {creator}
+      </Text>
+    </TouchableOpacity>
   );
 }
 
 function headerSubTab() {
-  return <Sub />;
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={DATA}
+        renderItem={({ item }) => (
+          <Item creator={item.creator} channel={item.channel} />
+        )}
+        keyExtractor={(item) => item.id}
+      />
+      <Text
+        style={{
+          paddingRight: 20,
+          paddingLeft: 20,
+          alignSelf: "center",
+          alignItems: "center",
+          color: "#065FDA",
+          fontWeight: "500",
+        }}
+      >
+        ALL
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   youtubers: {
-    margin: 10,
+    marginTop: 3,
+    marginBottom: 10,
+    marginLeft: 10,
+    alignItems: "center",
   },
 });
 
